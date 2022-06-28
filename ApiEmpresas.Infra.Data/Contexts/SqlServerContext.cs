8f84895f561fcc73b4a1d3abc4fe8651ecbe627a
smendescoti <sergio.coti@gmail.com>
2022-06-27 21:06:49 -0300

    first commit

---
+++ ApiEmpresas.Infra.Data/Contexts/SqlServerContext.cs
@@ -0,0 +1,36 @@
+﻿using ApiEmpresas.Domain.Entities;
+using ApiEmpresas.Infra.Data.Mappings;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiEmpresas.Infra.Data.Contexts
+{
+    /// <summary>
+    /// Classe de contexto do EntityFramework
+    /// </summary>
+    public class SqlServerContext : DbContext
+    {
+        //Método para fazer a conexão com o banco de dados
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=SQL8002.site4now.net;Initial Catalog=db_a8831c_apiempresas;User Id=db_a8831c_apiempresas_admin;Password=coti123456");
+        }
+
+        //método para adicionar as classes de mapeamento
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new EmpresaMapping());
+            modelBuilder.ApplyConfiguration(new FuncionarioMapping());
+            modelBuilder.ApplyConfiguration(new UsuarioMapping());
+        }
+
+        //propriedades DbSet para cada classe de entidade
+        public DbSet<Empresa> Empresa { get; set; }
+        public DbSet<Funcionario> Funcionario { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+    }
+}
